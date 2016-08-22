3e9e9dc673708467435be570bbd3b97390e2bc43
Fan Yang <missdeer@dfordsoft.com>
2016-08-22 14:36:45 +0800

    (*)dbrw WIP

--- src/derfla/execute/dbrw.cpp
+++ src/derfla/execute/dbrw.cpp
@@ -16,12 +16,75 @@ void DBRW::destroy()
     instance_ = nullptr;
 }
 
+bool DBRW::insertLFS(const QByteArray &icon, const QString &title, const QString &description, const QString &target, const QString &arguments, const QString workingDirectory, quint64 timestamp, quint64 lastModified, const QString &type)
+{
+    QSqlQuery query(db_);
+    query.prepare("INSERT INTO lfs (icon, title, description, target, arguments, working_directory, timestamp, last_modified, type) "
+        "VALUES (:icon, :title, :description, :target, :arguments, :working_directory, :timestamp, :last_modified, :type);");
+    // save to database
+    query.bindValue(":icon", icon);
+    query.bindValue(":title", title);
+    query.bindValue(":description", description);
+    query.bindValue(":target", target);
+    query.bindValue(":arguments", arguments);
+    query.bindValue(":working_directory", workingDirectory);
+    query.bindValue(":timestamp", timestamp);
+    query.bindValue(":last_modified", lastModified);
+    query.bindValue(":type", type);
+    return query.exec();
+}
+
 DBRW::DBRW()
 {
-
+    dbPath_ = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/cache.db";
+    if (!openDatabase())
+    {
+        qCritical() << "can't open cache database";
+    }
 }
 
 DBRW::~DBRW()
 {
+    if (db_.isValid() && db_.isOpen())
+    {
+        db_.close();
+    }
+}
 
+bool DBRW::createDatabase()
+{
+    db_ = QSqlDatabase::database(dbPath_, true);
+    if (!db_.isValid()) {
+        db_ = QSqlDatabase::addDatabase("QSQLITE", dbPath_);
+    }
+
+    db_.setDatabaseName(dbPath_);
+    if (!db_.isOpen())
+    {
+        if (!db_.open())
+        {
+            return false;
+        }
+    }
+    QSqlQuery query(db_);
+    return query.exec("CREATE TABLE lfs(id INTEGER PRIMARY KEY AUTOINCREMENT,icon TEXT, title TEXT, description TEXT,target TEXT, arguments TEXT, working_directory TEXT,timestamp DATETIME,last_modified DATETIME, type TEXT);");
+}
+
+bool DBRW::openDatabase()
+{
+    if (!QFile::exists(dbPath_))
+        return createDatabase();
+
+    db_ = QSqlDatabase::database(dbPath_, true);
+    if (!db_.isValid()) {
+        db_ = QSqlDatabase::addDatabase("QSQLITE", dbPath_);
+    }
+
+    db_.setDatabaseName(dbPath_);
+
+    if (db_.isOpen())
+    {
+        return true;
+    }
+    return db_.open();
 }
--- src/derfla/execute/dbrw.h
+++ src/derfla/execute/dbrw.h
@@ -9,14 +9,19 @@ class DBRW : public QObject
 public:
     static DBRW* instance();
     static void destroy();
+    bool insertLFS(const QByteArray& icon, const QString& title, const QString& description, const QString& target, const QString& arguments, const QString workingDirectory, quint64 timestamp, quint64 lastModified, const QString& type);
 signals:
 
 public slots:
 
 private:
     static DBRW *instance_;
+    QString dbPath_;
+    QSqlDatabase db_;
     DBRW();
     ~DBRW();
+    bool createDatabase();
+    bool openDatabase();
 };
 
 #endif // DBRW_H
