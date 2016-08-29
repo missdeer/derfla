058030f492a22198c9cf958ec7ea0201507a2ce4
Fan Yang <missdeer@dfordsoft.com>
2016-08-29 19:55:49 +0800

    (+)add some new skins

--- src/derfla/gui/derflawidget.cpp
+++ src/derfla/gui/derflawidget.cpp
@@ -25,7 +25,7 @@ DerflaWidget::DerflaWidget(QWidget *parent) :
 
     setFocusPolicy(Qt::ClickFocus);
     
-    if (!applySkin("black_glass_wide"))
+    if (!applySkin("derfla"))
     {
         qCritical() << "loading skin failed";
         return;
